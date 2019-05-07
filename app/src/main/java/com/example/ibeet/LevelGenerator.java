0e94461e3661dc67d1011ef87fed434fb302a53a
Mitro <mitro.lahonen@metropolia.fi>
2019-05-07 14:09:04 +0300

    Profile page speed and distance printing

---
+++ app/src/main/java/com/example/ibeet/LevelGenerator.java
@@ -0,0 +1,48 @@
+package com.example.ibeet;
+
+import android.content.SharedPreferences;
+
+public class LevelGenerator {
+    int currentLevel;
+    int totalXp;
+    int currentXp;
+    int neededXp;
+    MarkerHandler markerHandler = new MarkerHandler();
+
+    public LevelGenerator(){
+        totalXp = 0;
+        currentLevel = 0;
+        neededXp = 200;
+        currentXp = totalXp - neededXp;
+    }
+
+    public void calculateNewLevel(){
+        while (currentXp>neededXp){
+            isEnoughXp();
+            getXpNeeded();
+            resetCurrentXp();
+        }
+    }
+
+    public void isEnoughXp(){
+        if (currentXp>0){
+            currentLevel++;
+        } else{}
+    }
+
+    public void getXpNeeded(){
+        neededXp += currentLevel*200;
+    }
+
+    public void resetCurrentXp(){
+        currentXp = totalXp - neededXp;
+    }
+
+    public void setTotalXp(int totalDist){
+        this.totalXp = totalDist;
+    }
+
+    public int getCurrentLevel(){
+        return currentLevel;
+    }
+}
--- app/src/main/java/com/example/ibeet/ProfileActivity.java
+++ app/src/main/java/com/example/ibeet/ProfileActivity.java
@@ -5,14 +5,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class ProfileActivity extends AppCompatActivity {
-private TextView username, age, sessionDistance, sessionSpeed, allTimeDistance, lastSessionSpeed;
+private TextView username, age, sessionDistance, sessionSpeed, allTimeDistance, lastSessionSpeed, levelViewer;
 private Button logout;
 private SharedPreferences prefs;
+LevelGenerator levelGenerator = new LevelGenerator();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +29,7 @@ private SharedPreferences prefs;
         sessionSpeed = findViewById(R.id.thisSessionAvSpeedView);
         allTimeDistance = findViewById(R.id.allTimeDistanceView);
         lastSessionSpeed = findViewById(R.id.lastSessionAvSpeedView);
+        levelViewer = findViewById(R.id.levelView);
 
         prefs = getSharedPreferences("com.example.ibeet.DATES", MODE_PRIVATE);
 
@@ -37,16 +40,25 @@ private SharedPreferences prefs;
         username.setText(user);
         age.setText(ages);
 
-        //USERS TRACKER RECORDS DISPLAYED HERE:
+        //USER LEVEL
+        levelGenerator.setTotalXp((int) prefs.getFloat("allTimeDistanceTravelled", 0));
+        levelGenerator.calculateNewLevel();
+
+        //USERS TRACKER RECORDS AND LEVEL DISPLAYS HERE:
         float currSesDist = prefs.getFloat("sessionDistanceTravelled",0);
         float currSesSpeed = prefs.getFloat("thisSessionAverageSpeed", 0);
         float allDistance = prefs.getFloat("allTimeDistanceTravelled",0);
         float lastSesSpeed = prefs.getFloat("lastSessionAverageSpeed",0);
+        int userLevel = levelGenerator.getCurrentLevel();
 
         sessionDistance.setText(String.valueOf(currSesDist));
         sessionSpeed.setText(String.valueOf(currSesSpeed));
         allTimeDistance.setText(String.valueOf(allDistance));
         lastSessionSpeed.setText(String.valueOf(lastSesSpeed));
+        levelViewer.setText(String.valueOf(userLevel));
+
+        Log.d("Current level", String.valueOf(levelGenerator.getCurrentLevel()));
+
         //ON CLICK LOGS OUT OF PROFILE
         logout.setOnClickListener(new View.OnClickListener() {
             @Override
