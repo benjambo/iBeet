98e5a84ade4c9cd9af37c45dbea73361142bb7d8
benjamin <benjambo@metropolia.fi>
2019-05-08 17:02:09 +0300

    Publishing version with BottomNavigation!!!

--- app/src/main/java/com/example/ibeet/ProfileActivity.java
+++ app/src/main/java/com/example/ibeet/ProfileActivity.java
@@ -1,14 +1,20 @@
 package com.example.ibeet;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.io.File;
 
@@ -16,6 +22,8 @@ public class ProfileActivity extends AppCompatActivity {
 private TextView username, age, sessionDistance, sessionSpeed, allTimeDistance, lastSessionSpeed, levelViewer, levelProgressionViewer;
 private ProgressBar levelProgression;
 private Button logout;
+private long backPressedTime;
+private Toast backToast;
 private SharedPreferences prefs;
 LevelGenerator levelGenerator = new LevelGenerator();
 
@@ -35,6 +43,13 @@ LevelGenerator levelGenerator = new LevelGenerator();
         levelViewer = findViewById(R.id.levelView);
         levelProgression = findViewById(R.id.progressBarLevel);
         levelProgressionViewer = findViewById(R.id.levelProgressionView);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottomNavigationView);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+        Menu menu = bottomNav.getMenu();
+        MenuItem menuItem = menu.getItem(1);
+        menuItem.setChecked(true);
 
         prefs = getSharedPreferences("com.example.ibeet.DATES", MODE_PRIVATE);
 
@@ -82,4 +97,44 @@ LevelGenerator levelGenerator = new LevelGenerator();
             }
         });
     } //foo
+
+    //Navigation Setup
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    switch (menuItem.getItemId()) {
+                        case R.id.nav_food:
+                            Intent foodActivity = new Intent(ProfileActivity.this, FoodActivity.class);
+                            startActivity(foodActivity);
+                            break;
+
+                        case R.id.nav_profile:
+                            break;
+
+                        case R.id.nav_map:
+                            Intent mapActivity = new Intent(ProfileActivity.this, TrackerActivity.class);
+                            startActivity(mapActivity);
+                            break;
+                    }
+                    return false;
+                }
+            };
+
+    //Back press setup
+    @Override
+    public void onBackPressed() {
+        if (backPressedTime + 2000 > System.currentTimeMillis()) {
+            backToast.cancel();
+            Intent exitApp = new Intent(Intent.ACTION_MAIN);
+            exitApp.addCategory(Intent.CATEGORY_HOME);
+            exitApp.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(exitApp);
+            return;
+        } else {
+            backToast = Toast.makeText(ProfileActivity.this, "Press back again to exit", Toast.LENGTH_SHORT);
+            backToast.show();
+        }
+        backPressedTime = System.currentTimeMillis();
+    }
 }
